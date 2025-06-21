5fe8013e718ff170fffd5f6aa06cf36d8c06af71
Sergio Rodriguez Seoane <srodriguez@yepcode.io>
2025-06-21 02:26:46 +0200

    Add code syntax enhancer with copy button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import expressiveCode from "astro-expressive-code";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [
+    expressiveCode({
+      themes: ["dracula", "solarized-light"],
+    }),
+    mdx(),
+    sitemap(),
+  ],
 });
